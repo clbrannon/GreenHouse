b1608d238e328072fd8ecb5f777c65d14babf2ba
Chris <chrisl.brannon@gmail.com>
2022-12-20 15:12:29 -0600

    Correct Branch

---
+++ GreenHouse-Frontend/src/components/honeyDew.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { NavBar } from "./nav.js"
+import { RegisterForm } from "./register.js"
+import { TaskForm } from "./taskForm.js"
+import { PostList } from "./postList.js"
+import { Login } from "./Login.js"
+
+export const HoneyDew = () => {
+
+    return (
+        <>     
+          {/*}  <NavBar /> */}
+
+            <Login />
+            
+          {/* <RegisterForm />  */}
+          {/*  <TaskForm /> */}
+          {/*}  <PostList /> */}
+        </>
+    )
+ }
